5e40671032463555057626ac785755b73931229a
MadmouseKed <75257164+MadmouseKed@users.noreply.github.com>
2021-02-08 13:42:48 +0100

    0.0
    de eerste

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Wytze A. Ketel 1797080
+08/02/2021 13:29
+
+
+"""
+
+def genereerCode():
+    print("Ik genereer de code!")
+    code = []
+    
+
+
+    return code
+
+def spel():
+    print("Ik start het spel!")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    spel()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
